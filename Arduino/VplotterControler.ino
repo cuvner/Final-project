2f727ab9261132b17e5ebe5c41aa3e4848620896
cuvner <chriscourage699@hotmail.com>
2022-07-11 11:39:01 +0100

    added lcd write to  monitor

--- Arduino/VplotterControler.ino
+++ Arduino/VplotterControler.ino
@@ -2,19 +2,50 @@ int sensorValueX = 0 ;
 int sensorValueY = 0 ;
 int bValue = 0 ;
 
+
+// include the library code:
+#include <LiquidCrystal.h>
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup(){
   Serial.begin(9600) ;
   pinMode(2, INPUT);
   digitalWrite(2, HIGH);
+
+    // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // initialize the serial communications:
+  Serial.begin(9600);
 
   //hand shake protocol
 establishContact();
 }
 
 void loop(){
+
+// when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
+
+  
   if (Serial.available() > 0) {
+     // clear the screen
+    lcd.clear();
     // read the incoming byte:
     int inByte = Serial.read();
+    lcd.write(inByte);
     // read the sensor:
     sensorValueX = analogRead(A0);
     int xValue = map(sensorValueX, 0, 1023, 0, 255);
